add88a9c46d0b9c9a333829a85c0cce98a57b4f1
gayatri0907 <88106356+gayatri0907@users.noreply.github.com>
2022-03-06 14:20:02 +0530

    Add files via upload

---
+++ 01ListFunctions.py
@@ -0,0 +1,31 @@
+players=[]
+print(type(players))
+players.append('mohd salah')
+players.append('joe root')
+players.append('jos buttler')
+players.append('ben stokes')
+
+print(players)
+players.append('steve smith')
+print(players)
+players.insert(3,'eoin morgan')
+players.remove('joe root')
+print(players)
+players.sort()
+print(players)
+players.reverse()
+#players.clear()
+print(players)
+
+print(len(players))
+print(max(players))
+print(min(players))
+
+
+
+
+
+
+
+
+
